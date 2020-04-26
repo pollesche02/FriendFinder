a4663ce55e4187b40264cc596772635414243e49
Danielle Pollesche <dani@kcsburningaromas.com>
2020-04-25 21:57:01 -0700

    started my server.js and my friends.js files

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+    {
+        name: "Mr. Darcy",
+        photo: "https://images.app.goo.gl/hNRTd8kaV7d8yFF1A",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Elizabeth Bennet",
+        photo: "https://images.app.goo.gl/ygBL8UVN3irjJtdv9",
+        scores: [5,1,3,4,5,5,3,2,1,4]
+    },
+    {
+        name: "Jane Bennet",
+        photo: "https://images.app.goo.gl/TV8BZM1sdvGyFDiW8",
+        scores: [1,4,2,2,3,4,5,5,1,2]
+    },
+    {
+        name: "Mr. Bingley",
+        photo: "https://images.app.goo.gl/Z7iczwReuVmsNwYq7",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Mr. Wickham",
+        photo: "https://images.app.goo.gl/PZY2WwVp6WLrsRfe9",
+        scores: [1,2,4,5,5,2,2,1,3,1]
+    },
+    {
+        name: "Kitty Bennet",
+        photo: "https://images.app.goo.gl/zbQ7YsKiUE4dSQsa6",
+        scores: [5,3,4,5,2,1,3,4,2,3]
+    },
+
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require ("express");
+var bodyParser=require("body-parser");
+var path = require("path");
+var app = express();
+var port = 8080
+
+
+//start my code here for the bodyParser here
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type:"application/vnd.api+json"}));
+
+
+// now I need to code my routes for all my html pages
+app.use(express.static("/"));
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+
+
+app.listen(port, function() {
+    console.log("App is listening on port: http://localhost:" + port);
+});
+
+
