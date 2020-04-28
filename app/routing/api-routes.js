48f234d41a69081f0cfd0ff24dfe8fbcf664ebd6
Danielle Pollesche <dani@kcsburningaromas.com>
2020-04-28 11:46:45 -0700

    started api

---
+++ app/routing/api-routes.js
@@ -0,0 +1,67 @@
+var friends = require("../data/friends");
+
+// ROUTING
+
+
+module.exports = function(app) {
+  // API GET Requests
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  // API POST Requests
+
+  app.post("/api/friends", function(req, res) {
+      var totalDifference = 0;
+
+// find the best match  / compare the survey with every survey in the friends array
+
+ var bestMatch = {
+     name: "",
+     photo: "",
+     friendDifference: 1000
+ };
+
+ var userData = req.body;
+ var userName = userData.name;
+ var userScore = userData.score;
+
+ var b = userScore.map(function(item) {
+     return parseInt(item, 10);
+ });
+
+ userData = {
+     name: req.body.name,
+     photo: req.body.photo,
+     score: b
+ };
+ console.log("Name: " + userName);
+
+ var sum = b.reduce((a, b) => a + b, 0);
+ console.log("*************************");
+
+ for (var i = 0; i < friends.length, i++) {
+     console.log(friends[i].name);
+     totalDifference = 0;
+     console.log("Total Difference " + totalDifference);
+      console.log("Best match friend difference " + bestMatch.friendDifference);
+
+      var befriendScore = friends[i].score.reduce((a, b) => a + b, 0);
+      totalDifference += Math.abs(sum - befriendScore);
+      console.log("**********************" + totalDifference);
+
+  if 
+ }
+ 
+ 
+  });
+
+
+};
+
+
+
+// CRUD   create read update delete
+
+// methods POST GET PUT DELETE
