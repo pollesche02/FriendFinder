bd087870030c5b0218217e4c0acabed16bc10f08
Danielle Pollesche <dani@kcsburningaromas.com>
2020-05-01 14:30:25 -0700

    updated modal

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({type:"application/vnd.api+json"}));
 
 
 // now I need to code my routes for all my html pages
-app.use(express.static("./app/public"));
+app.use(express.static("./app/./public"));
 
 require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
