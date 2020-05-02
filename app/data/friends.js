04f7d572b2d1dc3d856850e0457d509e4a78130d
Danielle Pollesche <dani@kcsburningaromas.com>
2020-05-01 22:14:54 -0700

    code is working with pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friends = [
     {
         name: "Mr. Darcy",
-        photo: "https://images.app.goo.gl/hNRTd8kaV7d8yFF1A",
+        photo: "https://149349728.v2.pressablecdn.com/wp-content/uploads/2019/04/Screen-Shot-2019-04-22-at-11.17.11-PM.png",
         scores: [5,1,4,4,5,1,2,5,4,1]
     },
     {
         name: "Elizabeth Bennet",
-        photo: "https://images.app.goo.gl/ygBL8UVN3irjJtdv9",
+        photo: "https://i.pinimg.com/originals/12/97/79/129779d23de906307b2a706b9fdf974e.jpg",
         scores: [5,1,3,4,5,5,3,2,1,4]
     },
     {
         name: "Jane Bennet",
-        photo: "https://images.app.goo.gl/TV8BZM1sdvGyFDiW8",
+        photo: "https://images.blogthings.com/whichbennetsisterareyouquiz/jane.jpg",
         scores: [1,4,2,2,3,4,5,5,1,2]
     },
     {
         name: "Mr. Bingley",
-        photo: "https://images.app.goo.gl/Z7iczwReuVmsNwYq7",
+        photo: "https://66.media.tumblr.com/164e33e7c2c37e0dd33ffa5309d7ad6b/tumblr_inline_o5j2vtKFr71qlr65v_500.jpg",
         scores: [5,1,4,4,5,1,2,5,4,1]
     },
     {
         name: "Mr. Wickham",
-        photo: "https://images.app.goo.gl/PZY2WwVp6WLrsRfe9",
+        photo: "https://img4.bdbphotos.com/images/500x250/b/a/ba3afd4dz0l8lz.jpg?skj2io4l",
         scores: [1,2,4,5,5,2,2,1,3,1]
     },
     {
         name: "Kitty Bennet",
-        photo: "https://images.app.goo.gl/zbQ7YsKiUE4dSQsa6",
+        photo: "https://i.pinimg.com/originals/44/9b/16/449b1661a51235bf237bc52c920564dc.jpg",
         scores: [5,3,4,5,2,1,3,4,2,3]
     },
 
