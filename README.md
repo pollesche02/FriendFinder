e85d9c40fb37cc9db5e9bba1dcebb3392d02e370
Danielle Pollesche <dani@kcsburningaromas.com>
2020-05-02 21:53:01 -0700

    finished working all my css

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # FriendFinder
 
-If you want to play the game you can simply click here: https://github.com/pollesche02/FriendFinder/settings
+If you want to play the game you can simply click here: 
 
  
  This is a Match Finder Application. This application will use Node.js and Express Package
 
  This app will use two different HTML files, a home.html and a survey,html. It will also use two api files an api-routes.js and an html-routes.js
+
+ Instructions to use: 
+
+ 1. Diploy the link
+ 2. On this page you want to click on the button thats requesting you to start the survey
+ 3. Type in your name 
+ 4. Begin going through each question and answer them. The answering system will go 1 - 5; 5 being the most agreeable.
+ 5. Once you have completed all 10 questions that you will want to hit the submit button down at the bottom. 
+ 6. This will show you a picture of your perfect match.
+
+ Through out this specific activity I was able to learn about Heroku and how to change my thought process on API's. Typically from this point
+ we have been using API's from other sources this activity we had to in a way use our own code as the API... 
+
+ Please feel free to review my LinkedIn page @    https://www.linkedin.com/in/danielle-pollesche-b397332b/
+
+ 
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,33 @@
 var friends = [
     {
         name: "Mr. Darcy",
-        photo: "https://149349728.v2.pressablecdn.com/wp-content/uploads/2019/04/Screen-Shot-2019-04-22-at-11.17.11-PM.png",
-        scores: [5,1,4,4,5,1,2,5,4,1]
+        photo: "https://i.pinimg.com/originals/f3/52/37/f35237f4f4286db4183c7ef7f44d8822.gif",
+        scores: [5, 1, 4, 5, 5, 1, 2, 5, 4, 1]
     },
     {
         name: "Elizabeth Bennet",
-        photo: "https://i.pinimg.com/originals/12/97/79/129779d23de906307b2a706b9fdf974e.jpg",
-        scores: [5,1,3,4,5,5,3,2,1,4]
+        photo: "https://pbs.twimg.com/profile_images/930538338607599621/9p30tt8G_200x200.jpg",
+        scores: [5, 1, 3, 4, 5, 5, 3, 2, 1, 4]
     },
     {
         name: "Jane Bennet",
-        photo: "https://images.blogthings.com/whichbennetsisterareyouquiz/jane.jpg",
-        scores: [1,4,2,2,3,4,5,5,1,2]
+        photo: "https://openpsychometrics.org/tests/characters/test-resources/pics/PP/8.jpg",
+        scores: [1, 4, 2, 5, 3, 4, 5, 5, 1, 2]
     },
     {
         name: "Mr. Bingley",
-        photo: "https://66.media.tumblr.com/164e33e7c2c37e0dd33ffa5309d7ad6b/tumblr_inline_o5j2vtKFr71qlr65v_500.jpg",
-        scores: [5,1,4,4,5,1,2,5,4,1]
+        photo: "https://reginajeffers.files.wordpress.com/2018/06/3220924_tml.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 5, 1]
     },
     {
         name: "Mr. Wickham",
-        photo: "https://img4.bdbphotos.com/images/500x250/b/a/ba3afd4dz0l8lz.jpg?skj2io4l",
-        scores: [1,2,4,5,5,2,2,1,3,1]
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQRe5Y897QxYrJ_crrl7HdQLC2zuc5119UDbOQVZxpYeeq1-8sr&usqp=CAU",
+        scores: [1, 5, 4, 5, 5, 2, 2, 1, 5, 1]
     },
     {
         name: "Kitty Bennet",
         photo: "https://i.pinimg.com/originals/44/9b/16/449b1661a51235bf237bc52c920564dc.jpg",
-        scores: [5,3,4,5,2,1,3,4,2,3]
+        scores: [5, 3, 4, 5, 5, 1, 5, 4, 2, 3]
     },
 
 ];
